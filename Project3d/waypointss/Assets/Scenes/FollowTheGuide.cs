be53fa862ff83e74f9b07b6848c454bac1f9c364
Juan Siverio <alu0100585704@ull.edu.es>
2022-11-28 14:35:01 +0000

    waypoints actualizados

--- Project3d/waypointss/Assets/Scenes/FollowTheGuide.cs
+++ Project3d/waypointss/Assets/Scenes/FollowTheGuide.cs
@@ -2,31 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class FollowTheGuide : MonoBehaviour
 {
     [SerializeField]
     private Transform goal_;
     [SerializeField]
     private float speed_  = 2f;
-    v
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+/// <summary>
+/// Start is called on the frame when a script is enabled just before
+/// any of the Update methods is called the first time.
+/// </summary>
+void Start()
+{
+    if (goal_ == null)        
+        goal_= GameObject.Find("Guide").transform;
+    
+    
+}   
+void FollowAndTranslateTo(Transform draft)
+{
+    Vector3 goal = draft.position - transform.position;
+    //transform.LookAt(draft.position);
+
+    //Sobre todos los ejes suavizado. Notar que aquí LockRotation utiliza un vector dirección para saber hacia donde rotar,y no la posicion en las coordenadas del mundo
+    //del objeto a mirar, como si sucede en LookAt()
+    
+    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(goal), speed_ * Time.deltaTime);
+    
+    
+    transform.Translate(goal * Time.deltaTime);
+    Debug.DrawRay(transform.position, goal, Color.red);    
+}
 /// <summary>
 /// LateUpdate is called every frame, if the Behaviour is enabled.
 /// It is called after all Update functions have been called.
 /// </summary>
-void LateUpdate()
+void Update()
 {
-    Vector3 newPos;
-    newPos = 
+    FollowAndTranslateTo(goal_);
 }
     
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
