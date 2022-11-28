ad0a42b9aee6ac43fd14bd433065ea8b9135114d
Juan Siverio <alu0100585704@ull.edu.es>
2022-11-28 12:02:36 +0000

    cinematic and waypoints

--- Project3d/waypointss/Assets/Scenes/FollowTheGuide.cs
+++ Project3d/waypointss/Assets/Scenes/FollowTheGuide.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    [SerializeField]
+    private Transform goal_;
+    [SerializeField]
+    private float speed_  = 2f;
+    v
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+/// <summary>
+/// LateUpdate is called every frame, if the Behaviour is enabled.
+/// It is called after all Update functions have been called.
+/// </summary>
+void LateUpdate()
+{
+    Vector3 newPos;
+    newPos = 
+}
+    
     // Update is called once per frame
     void Update()
     {
