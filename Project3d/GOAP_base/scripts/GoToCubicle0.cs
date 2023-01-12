34dae752c2dcab57ce66f31f748a8e348aa777c4
Juan Siverio <alu0100585704@ull.edu.es>
2023-01-12 08:42:16 +0000

    agregado inicio goap_base

---
+++ Project3d/GOAP_base/scripts/GoToCubicle0.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToCubicle : GAction
+{
+
+    public override bool PrePerform()
+    {
+        target = inventory.FindItemWithTag("Cubicle");
+        if (target == null)
+            return false;
+
+        return true;
+    }
+
+    public override bool PostPerform()
+    {
+        GWorld.Instance.GetWorld().ModifyState("treatingPatient", 1);
+        GWorld.Instance.AddCublicle(target);
+        inventory.RemoveItem(target);
+        GWorld.Instance.GetWorld().ModifyState("freeCubicle", 1);
+        return true;
+    }
+}
